e9447c6ad418c70c9dab60dae83cad1bce61750b
SibomanaEdouard <sibomanaedouard974@gmail.com>
2024-11-19 06:38:53 +0200

    welcome message

--- app/main.py
+++ app/main.py
@@ -14,3 +14,6 @@ app = FastAPI(on_startup=[init_db])
 app.include_router(patients.router, prefix="/api/v1")
 # app.include_router(appointments.router, prefix="/api")
 # app.include_router(billing.router, prefix="/api")
+@app.get("/")
+async def welcome():
+    return {"message": "Welcome to the fastapi project"}
